bd1b77efd751eaddd5216ae8c9303c0c26f5249d
ceresG <46253379+ceresG@users.noreply.github.com>
2022-02-19 14:24:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ var config = {
    // },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "点这里",
+        turn_on: "点一下",
         play: "音乐",
         bannar_coming: "再点",
         balloons_flying: "好像少点东西",
